23c9de92642e230c583a6a3b6fdee092b1a89da6
Chaal Pritam <chaalpritam@gmail.com>
2026-05-14 23:29:05 +0530

    Initial Commit

---
+++ tribe-insta/tribe_instaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tribe_instaApp.swift
+//  tribe-insta
+//
+//  Created by chaalpritam on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct tribe_instaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
